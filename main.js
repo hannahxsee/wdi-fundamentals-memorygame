19b5598b52ec28b87b91fadb9cf53e658aa0b207
hannahxsee <60167489+hannahxsee@users.noreply.github.com>
2020-02-18 18:21:01 -0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+console.log("Up and running!");
+
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThree = "king";
+let cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
+
+String[] cards = {"queen", "queen", "king", "king"};
+
+String[] cardsInPlay = {"cardOne", "cardTwo", "Ford", "Mazda"} 
+cardOne.push('queen') cardTwo.push('king');
+
+String[] cardOne = {"queen"};
+
+console.log("User flipped queen")
+
+String[] cardTwo = {"king"};
+
+console.log("User flipped king")
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+
+const myArray = [
+{
+color: 'red',
+flower: 'rose',
+petals: 20
+},
+{
+color: 'blue',
+flower: 'violet',
+petals: 6
+}
+];
+
+for (let i = 0; i < arrayName.length; i++) {
+    // Logic here
+}
+
+let newListItem = document.cardElement('img');
+let newListItem = document.cardElement('img');
+let newListItem = document.cardElement('img');
+let newListItem = document.cardElement('img');
